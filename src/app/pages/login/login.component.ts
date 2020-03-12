3e0f50dcc84979c03e82ebdd13e02fd92ed860ee
Alberto Silva <silvaguzmanalberto@gmail.com>
2020-03-12 14:52:40 -0600

    Create auth service with dummy data

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Alert } from 'src/app/classes/alert';
 import { AlertType } from 'src/app/enums/alert-type.enum';
 import { AlertService } from 'src/app/services/alert.service';
 import { LoadingService } from 'src/app/services/loading.service';
+import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
+  private subscriptions: Subscription[] = [];
+  private returnUrl: string;
   public loginForm: FormGroup;
+
   constructor(
     private fb: FormBuilder,
     private alertService: AlertService,
-    private loadingService: LoadingService) {
+    private loadingService: LoadingService,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute) {
     this.createForm();
   }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/chat';
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe);
   }
 
   private createForm(): void {
@@ -31,18 +45,25 @@ export class LoginComponent implements OnInit {
   }
 
   public submit(): void {
-    this.loadingService.isLoading.next(true);
-
     if ( this.loginForm.valid ) {
+      this.loadingService.isLoading.next(true);
       const {email, password} = this.loginForm.value;
-      console.log(`Email: ${email} Password: ${password}`);
+
+      this.subscriptions.push(
+        this.auth.login(email, password).subscribe(success => {
+          if ( success ) {
+            this.router.navigateByUrl(this.returnUrl);
+          } else {
+            this.loadingService.isLoading.next(false);
+          }
+        })
+      );
+
       this.loadingService.isLoading.next(false);
     } else {
       const failedLoginAlert = new Alert('Your email or password were invalid, try again.', AlertType.Danger);
-      setTimeout(() => {
-        this.loadingService.isLoading.next(false);
-        this.alertService.alerts.next(failedLoginAlert);
-      }, 2000);
+      this.loadingService.isLoading.next(false);
+      this.alertService.alerts.next(failedLoginAlert);
     }
   }
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Alert } from 'src/app/classes/alert';
 import { AlertType } from 'src/app/enums/alert-type.enum';
 import { AlertService } from 'src/app/services/alert.service';
+import { Subscription } from 'rxjs';
+import { LoadingService } from 'src/app/services/loading.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
 
   public signupForm: FormGroup;
-  constructor(private fb: FormBuilder, private alertService: AlertService) {
+  private subscriptions: Subscription[] = [];
+  
+  constructor(
+    private fb: FormBuilder,
+    private alertService: AlertService,
+    private loadingService: LoadingService,
+    private auth: AuthService,
+    private router: Router) {
     this.createForm();
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe);
   }
 
   private createForm(): void {
@@ -31,7 +46,15 @@ export class SignupComponent implements OnInit {
   public submit(): void {
     if (this.signupForm.valid) {
       const {firstName, lastName, email, password} = this.signupForm.value;
-      console.log(`Email: ${email} Password: ${password} Name: ${firstName} Last Name: ${lastName}`);
+      this.subscriptions.push(
+        this.auth.signup(firstName, lastName, email, password).subscribe(success => {
+          if ( success ) {
+            this.router.navigateByUrl('/chat');
+          }
+          this.loadingService.isLoading.next(false);
+        })
+      );
+      
     } else {
       const failedSignedAlert = new Alert('Please enter a valid name, emai and passwod, try again', AlertType.Danger);
       this.alertService.alerts.next(failedSignedAlert);
